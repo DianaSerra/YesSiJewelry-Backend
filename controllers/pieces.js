75288f6166fd1d97e37977a4368d8e60df8fdd23
DianaSerra <dianaiserra@hotmail.com>
2020-08-27 21:39:32 -0400

    fixed add piece id assignment

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -9,40 +9,37 @@ export const all_pieces = (req, res) => {
     });
 };
 
-export const add_piece = (req, res, next) => {
-  const new_piece_id = Piece.find()
+export const add_piece = async (req, res, next) => {
+  //Gets id of last piece and adds one to make sure ID is unique
+  Piece.find({})
     .sort({ id: "desc" })
-    .lean()
-    .exec(function (err, pieces) {
-      consnole.log(pieces[0]);
-      return pieces[0];
+    .limit(1)
+    .then(function (doc) {
+      const newPieceId = doc[0].id + 1;
+      var newPiece = new Piece({
+        id: newPieceId,
+        description: req.body.description,
+        price: req.body.price,
+        sold: req.body.sold || false,
+        buyer: req.body.buyer || "",
+        category: req.body.category || "",
+        metal: req.body.metal || "",
+        stone: req.body.stone || "",
+        length: req.body.length || 0,
+        weight: req.body.weight || 0,
+        appraisal: req.body.appraisal || false,
+        notes: req.body.notes || "",
+        instaURL: req.body.instaURL || "",
+        fbURL: req.body.fbURL || "",
+      });
+
+      Piece.create(newPiece, (err, item) => {
+        if (err) {
+          console.log(err);
+        } else {
+          //TODO: change this to the "edit item" route at some point
+          res.redirect("/pieces.json");
+        }
+      });
     });
-
-  console.log(new_piece_id);
-
-  var newPiece = new Piece({
-    id: new_piece_id,
-    description: req.body.description,
-    price: req.body.price,
-    sold: req.body.sold || false,
-    buyer: req.body.buyer || "",
-    category: req.body.category || "",
-    metal: req.body.metal || "",
-    stone: req.body.stone || "",
-    length: req.body.length || 0,
-    weight: req.body.weight || 0,
-    appraisal: req.body.appraisal || false,
-    notes: req.body.notes || "",
-    instaURL: req.body.instaURL || "",
-    fbURL: req.body.fbURL || "",
-  });
-
-  Piece.create(newPiece, (err, item) => {
-    if (err) {
-      console.log(err);
-    } else {
-      //TODO: change this to the "edit item" route at some point
-      res.redirect("/pieces.json");
-    }
-  });
 };
